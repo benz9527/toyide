00847a5e46fdf26e2323e5e9a03ae26281b86585
Ben.Zheng <benzheng.6dev9527@outlook.com>
2022-08-31 14:53:51 +0800

    Updated the color scheme.

--- lua/toyide/colorscheme.lua
+++ lua/toyide/colorscheme.lua
@@ -1,7 +1,8 @@
-local colorscheme = "default"
-
-local cs_status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not cs_status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
-end
+vim.cmd [[
+try
+    colorscheme darkplus
+catch /^Vim\%((\a\+)\)\=:E185/
+    colorscheme default
+    set background=dark
+endtry
+]]
