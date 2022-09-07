a31c59a7d9136eae6b6576911f27806c948a23e9
Ben.Zheng <benzheng.6dev9527@outlook.com>
2022-09-07 20:21:52 +0800

    Added disabled builtin plugins.

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("toyide.core.options")
+require("toyide.core.disable_builtin_plugins")
 require("toyide.core.keymappings").load_keymappings()
 require("toyide.plugins")
 require("toyide.colorscheme")
